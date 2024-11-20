1a66a34398b0bcc52d6db3f427b6bfbdffa82476
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2024-11-20 19:18:43 +0530

    Add initial core implementation for the tool

---
+++ xsd-core/src/main/java/io/ballerina/xsdtorecordconverter/XSDVisitor.java
@@ -0,0 +1,422 @@
+package io.ballerina.xsdtorecordconverter;
+
+import io.ballerina.compiler.syntax.tree.SyntaxKind;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class XSDVisitor implements IXSDVisitor {
+    public static final String PUBLIC = "public";
+    public static final String WHITESPACE = " ";
+    public static final String EQUAL = "=";
+    public static final String SEMICOLON = ";";
+    public static final String RECORD = "record";
+    public static final String TYPE = "type";
+    public static final String NAME = "name";
+    public static final String DEFAULT = "default";
+    public static final String FIXED = "fixed";
+    public static final String NEW_LINE = "\n";
+    public static final String QUOTATION_MARK = "\"";
+    public static final String OPEN_BRACES = "{";
+    public static final String CLOSE_BRACES = "}";
+    public static final String OPEN_SQUARE_BRACKET = "[";
+    public static final String CLOSE_SQUARE_BRACKET = "]";
+    public static final String IMPORT = "import";
+    public static final String ELEMENT = "element";
+    public static final String COMPLEX_TYPE = "complexType";
+    public static final String SIMPLE_TYPE = "simpleType";
+    public static final String SEQUENCE = "sequence";
+    public static final String CHOICE = "choice";
+    public static final String ATTRIBUTE = "attribute";
+    public static final String COMPLEX_CONTENT = "complexContent";
+    public static final String SIMPLE_CONTENT = "simpleContent";
+    public static final String EXTENSION = "extension";
+    public static final String ENUMERATION = "enumeration";
+    public static final String ENUM = "enum";
+    public static final String BASE = "base";
+    public static final String ZERO = "0";
+    public static final String ONE = "1";
+    public static final String QUESTION_MARK = "?";
+    public static final String MAX_OCCURS = "maxOccurs";
+    public static final String MIN_OCCURS = "minOccurs";
+    public static final String UNBOUNDED = "unbounded";
+    public static final String REQUIRED = "required";
+    public static final String USE = "use";
+    public static final String VALUE = "value";
+    public static final String COMMA = ",";
+    public static final String COLON = ":";
+    public static final String TIME = "time";
+    public static final String DATE_TIME = "dateTime";
+    public static final String DATE = "date";
+    public static final String G_YEAR_MONTH = "gYearMonth";
+    public static final String G_YEAR = "gYear";
+    public static final String STRING = "string";
+    public static final String INTEGER = "integer";
+    public static final String LONG = "long";
+    public static final String NEGATIVE_INTEGER = "negativeInteger";
+    public static final String NON_POSITIVE_INTEGER = "nonPositiveInteger";
+    public static final String POSITIVE_INTEGER = "positiveInteger";
+    public static final String SHORT = "short";
+    public static final String UNSIGNED_LONG = "unsignedLong";
+    public static final String UNSIGNED_INT = "unsignedInt";
+    public static final String UNSIGNED_SHORT = "unsignedShort";
+    public static final String UNSIGNED_BYTE = "unsignedByte";
+    public static final String INT = "int";
+    public static final String BASE64_BINARY = "base64Binary";
+    public static final String BYTE_ARRAY = "byte[]";
+    public static final String BALLERINA_XML_DATA_MODULE = "ballerina/data.xmldata";
+    public static final String ATTRIBUTE_ANNOTATION = "@xmldata:Attribute";
+    public static final String TYPE_NAME_SUFFIX = "123";
+
+    private final ArrayList<String> imports = new ArrayList<>();
+    private final LinkedHashMap<String, String> extensions = new LinkedHashMap<>();
+
+    @Override
+    public String visit(Element element) {
+        Node node = element.getNode();
+        StringBuilder builder = new StringBuilder();
+        builder.append(PUBLIC).append(WHITESPACE).append(TYPE).append(WHITESPACE);
+        if (!node.hasChildNodes()) {
+            return handleSingleElementNode(node, builder);
+        }
+        return handleElementsWithChildNodes(node, builder);
+    }
+
+    @Override
+    public String visit(Element element, boolean subElement) {
+        Node node = element.getNode();
+        StringBuilder builder = new StringBuilder();
+        Node nameNode = node.getAttributes().getNamedItem(NAME);
+        Node typeNode = node.getAttributes().getNamedItem(TYPE);
+        handleFixedValues(node, builder, typeNode);
+        handleMaxOccurrences(node, builder);
+        builder.append(nameNode.getNodeValue());
+        handleMinOccurrences(node, builder);
+        handleDefaultValues(node, builder, typeNode);
+        builder.append(SEMICOLON);
+        return builder.toString();
+    }
+
+    private void handleFixedValues(Node node, StringBuilder builder, Node typeNode) {
+        Node fixedNode = node.getAttributes().getNamedItem(FIXED);
+        if (fixedNode != null) {
+            builder.append(generateFixedValue(deriveType(typeNode), fixedNode.getNodeValue())).append(WHITESPACE);
+        } else {
+            builder.append(deriveType(typeNode)).append(WHITESPACE);
+        }
+    }
+
+    private void handleDefaultValues(Node node, StringBuilder builder, Node typeNode) {
+        Node defaultNode = node.getAttributes().getNamedItem(DEFAULT);
+        if (defaultNode != null) {
+            builder.append(generateDefaultValue(deriveType(typeNode), defaultNode.getNodeValue()));
+        }
+    }
+
+    private static void handleMinOccurrences(Node node, StringBuilder builder) {
+        Node minOccurrence = node.getAttributes().getNamedItem(MIN_OCCURS);
+        if (minOccurrence != null) {
+            if (minOccurrence.getNodeValue().equals(ZERO)) {
+                builder.append(QUESTION_MARK);
+            }
+        }
+    }
+
+    private static void handleMaxOccurrences(Node node, StringBuilder builder) {
+        Node maxOccurrence = node.getAttributes().getNamedItem(MAX_OCCURS);
+        if (maxOccurrence != null) {
+            if (maxOccurrence.getNodeValue().equals(UNBOUNDED)) {
+                builder.append(OPEN_SQUARE_BRACKET).append(CLOSE_SQUARE_BRACKET).append(WHITESPACE);
+            } else if (!maxOccurrence.getNodeValue().equals(ONE)) {
+                builder.append(OPEN_SQUARE_BRACKET).append(maxOccurrence.getNodeValue())
+                        .append(CLOSE_SQUARE_BRACKET).append(WHITESPACE);
+            }
+        }
+    }
+
+    private void handleFixedValues(StringBuilder builder, Node typeNode, Node fixedNode) {
+        if (fixedNode != null) {
+            builder.append(generateFixedValue(deriveType(typeNode), fixedNode.getNodeValue())).append(WHITESPACE);
+        } else {
+            builder.append(deriveType(typeNode)).append(WHITESPACE);
+        }
+    }
+
+    private static void handleMaxOccurrences(StringBuilder builder, Node maxOccurrence) {
+
+    }
+
+    private String handleElementsWithChildNodes(Node node, StringBuilder builder) {
+        Node nameNode = node.getAttributes().getNamedItem(NAME);
+        Node typeNode = node.getAttributes().getNamedItem(TYPE);
+        if (typeNode != null && typeNode.getNodeValue().equals(nameNode.getNodeValue())) {
+            builder.append(nameNode.getNodeValue()).append(TYPE_NAME_SUFFIX).append(WHITESPACE);
+        } else if (typeNode == null) {
+            builder.append(nameNode.getNodeValue()).append(WHITESPACE);
+        }
+        if (typeNode != null) {
+            builder.append(deriveType(typeNode)).append(SEMICOLON).append(NEW_LINE);
+        }
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            IComponent component = XSDFactory.generateComponents(node.getChildNodes().item(i));
+            if (component != null) {
+                component.setSubType(true);
+                builder.append(component.accept(this));
+            }
+        }
+        return builder.toString();
+    }
+
+    public String generateFixedValue(String type, String value) {
+        StringBuilder builder = new StringBuilder();
+        switch (type) {
+            case INT -> builder.append(value).append(WHITESPACE);
+            case STRING -> builder.append(QUOTATION_MARK).append(value).append(QUOTATION_MARK).append(WHITESPACE);
+        }
+        return builder.toString();
+    }
+
+    public String generateDefaultValue(String type, String value) {
+        StringBuilder builder = new StringBuilder().append(WHITESPACE).append(EQUAL).append(WHITESPACE);
+        switch (type) {
+            case INT -> builder.append(value);
+            case STRING -> builder.append(QUOTATION_MARK).append(value).append(QUOTATION_MARK);
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public String visit(ComplexType element) {
+        Node node = element.getNode();
+        StringBuilder builder = new StringBuilder();
+        builder.append(PUBLIC).append(WHITESPACE).append(TYPE).append(WHITESPACE);
+        Node nameNode = node.getAttributes().getNamedItem(NAME);
+        if (nameNode != null) {
+            builder.append(nameNode.getNodeValue()).append(WHITESPACE);
+            builder.append(RECORD).append(WHITESPACE);
+            builder.append(OPEN_BRACES);
+        }
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            if (node.getChildNodes().item(i).getNodeType() == Node.ELEMENT_NODE) {
+                Node compositor = node.getChildNodes().item(i);
+                switch (compositor.getLocalName()) {
+                    case SEQUENCE -> builder.append(visitSequence(compositor));
+                    case CHOICE -> builder.append(visitChoice(compositor));
+                    case ATTRIBUTE -> builder.append(visitAttribute(compositor));
+                    case COMPLEX_CONTENT, SIMPLE_CONTENT -> builder.append(visitComplexContent(compositor));
+                }
+            }
+        }
+        builder.append(CLOSE_BRACES).append(SEMICOLON);
+        return builder.toString();
+    }
+
+    @Override
+    public String visit(ComplexType element, boolean isSubType) {
+        Node node = element.getNode();
+        StringBuilder builder = new StringBuilder();
+        builder.append(RECORD).append(WHITESPACE).append(OPEN_BRACES).append(WHITESPACE);
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            if (node.getChildNodes().item(i).getNodeType() == Node.ELEMENT_NODE) {
+                Node compositor = node.getChildNodes().item(i);
+                switch (compositor.getLocalName()) {
+                    case SEQUENCE -> builder.append(visitSequence(compositor));
+                    case CHOICE -> builder.append(visitChoice(compositor));
+                    case ATTRIBUTE -> builder.append(visitAttribute(compositor));
+                    case COMPLEX_CONTENT, SIMPLE_CONTENT -> builder.append(visitComplexContent(compositor));
+                }
+            }
+        }
+        builder.append(CLOSE_BRACES).append(SEMICOLON);
+        return builder.toString();
+    }
+
+    public String visitComplexContent(Node node) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            Node element = node.getChildNodes().item(i);
+            if (element.getNodeType() == Node.ELEMENT_NODE) {
+                if (element.getLocalName().equals(EXTENSION)) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(PUBLIC).append(WHITESPACE).append(TYPE).append(WHITESPACE);
+                    String base = element.getAttributes().getNamedItem(BASE).getNodeValue();
+                    builder.append(base).append(WHITESPACE).append(base).append(SEMICOLON);
+                    stringBuilder.append(base).append(OPEN_BRACES);
+                    stringBuilder.append(visitExtension(element));
+                    stringBuilder.append(CLOSE_BRACES).append(SEMICOLON);
+                    extensions.put(base, stringBuilder.toString());
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public String visitExtension(Node node) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            Node element = node.getChildNodes().item(i);
+            if (element.getNodeType() == Node.ELEMENT_NODE) {
+                builder.append(visitAttribute(element));
+            }
+        }
+        return builder.toString();
+    }
+
+    public String visitChoice(Node node) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            Node element = node.getChildNodes().item(i);
+            if (element.getNodeType() == Node.ELEMENT_NODE) {
+                if (element.getLocalName().equals(SEQUENCE)) {
+                    builder.append(visitSequence(element));
+                } else {
+                    Node nameNode = element.getAttributes().getNamedItem(NAME);
+                    Node typeNode = element.getAttributes().getNamedItem(TYPE);
+                    builder.append(deriveType(typeNode)).append(WHITESPACE);
+                    builder.append(nameNode.getNodeValue()).append(QUESTION_MARK).append(SEMICOLON);
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public String visitAttribute(Node attribute) {
+        StringBuilder builder = new StringBuilder();
+        this.addImports(BALLERINA_XML_DATA_MODULE);
+        builder.append(ATTRIBUTE_ANNOTATION).append(WHITESPACE);
+        Node nameNode = attribute.getAttributes().getNamedItem(NAME);
+        Node typeNode = attribute.getAttributes().getNamedItem(TYPE);
+        Node defaultNode = attribute.getAttributes().getNamedItem(DEFAULT);
+        Node fixedNode = attribute.getAttributes().getNamedItem(FIXED);
+        handleFixedValues(builder, typeNode, fixedNode);
+        builder.append(nameNode.getNodeValue());
+        Node attributeType = attribute.getAttributes().getNamedItem(USE);
+        if (attributeType != null) {
+            if (!attributeType.getNodeValue().equals(REQUIRED)) {
+                builder.append(QUESTION_MARK);
+            }
+        }
+        if (defaultNode != null) {
+            builder.append(generateDefaultValue(deriveType(typeNode), defaultNode.getNodeValue()));
+        }
+        builder.append(SEMICOLON);
+        return builder.toString();
+    }
+
+    public String visitSequence(Node node) {
+        StringBuilder builder = new StringBuilder();
+        for (int j = 0; j < node.getChildNodes().getLength(); j++) {
+            IComponent component = XSDFactory.generateComponents(node.getChildNodes().item(j));
+            if (component != null) {
+                component.setSubType(true);
+                builder.append(component.accept(this));
+            }
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public String visit(SimpleType element) {
+        StringBuilder builder = new StringBuilder();
+        Node nameNode = element.getNode().getAttributes().getNamedItem(NAME);
+        for (int i = 0; i < element.getNode().getChildNodes().getLength(); i++) {
+            Node simpleTypeNode = element.getNode().getChildNodes().item(i);
+            StringBuilder stringBuilder = new StringBuilder();
+            if (simpleTypeNode.getNodeType() == Node.ELEMENT_NODE) {
+                boolean enumeration = hasEnumerations(simpleTypeNode, stringBuilder);
+                if (enumeration) {
+                    builder.append(PUBLIC).append(WHITESPACE).append(ENUM).append(WHITESPACE);
+                    builder.append(nameNode.getNodeValue()).append(WHITESPACE).append(OPEN_BRACES);
+                    builder.append(stringBuilder.substring(0, stringBuilder.length() - 1));
+                    builder.append(CLOSE_BRACES).append(SEMICOLON);
+                } else {
+                    builder.append(PUBLIC).append(WHITESPACE).append(TYPE).append(WHITESPACE);
+                    builder.append(nameNode.getNodeValue()).append(WHITESPACE);
+                    Node typeNode = simpleTypeNode.getAttributes().getNamedItem(BASE);
+                    builder.append(deriveType(typeNode)).append(SEMICOLON);
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    private static boolean hasEnumerations(Node simpleTypeNode, StringBuilder stringBuilder) {
+        boolean enumeration = false;
+        if (simpleTypeNode.hasChildNodes()) {
+            NodeList nodes = simpleTypeNode.getChildNodes();
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    if (node.getLocalName().equals(ENUMERATION)) {
+                        enumeration = true;
+                        stringBuilder.append(node.getAttributes()
+                                .getNamedItem(VALUE).getNodeValue()).append(COMMA);
+                    }
+                }
+            }
+        }
+        return enumeration;
+    }
+
+    private String handleSingleElementNode(Node element, StringBuilder builder) {
+        Node nameNode = element.getAttributes().getNamedItem(NAME);
+        if (nameNode != null) {
+            Node typeNode = element.getAttributes().getNamedItem(TYPE);
+            if (typeNode != null) {
+                String typeName = deriveType(typeNode);
+                if (typeName.equals(nameNode.getNodeValue())) {
+                    builder.append(nameNode.getNodeValue()).append(TYPE_NAME_SUFFIX).append(WHITESPACE);
+                } else {
+                    builder.append(nameNode.getNodeValue()).append(WHITESPACE);
+                }
+                builder.append(typeGenerator(typeName));
+            } else {
+                builder.append(SyntaxKind.ANYDATA_KEYWORD.stringValue());
+            }
+            builder.append(SEMICOLON);
+            return builder.toString();
+        }
+        return null;
+    }
+
+    public String deriveType(Node node) {
+        String derivedType = node.getNodeValue().contains(COLON) ?
+                node.getNodeValue().substring(node.getNodeValue().indexOf(COLON) + 1) : node.getNodeValue();
+        return typeGenerator(derivedType);
+    }
+
+    public ArrayList<String> getImports() {
+        return imports;
+    }
+
+    public void addImports(String module) {
+        String importDeclaration = IMPORT + WHITESPACE + module + SEMICOLON;
+        if (!imports.contains(importDeclaration)) {
+            this.imports.add(importDeclaration);
+        }
+    }
+
+    public LinkedHashMap<String, String> getExtensions() {
+        return extensions;
+    }
+
+    public static String typeGenerator(String typeName) {
+        switch (typeName) {
+            case TIME, DATE_TIME, DATE, G_YEAR_MONTH, G_YEAR, STRING -> {
+                return STRING;
+            }
+            case INTEGER, LONG, NEGATIVE_INTEGER, NON_POSITIVE_INTEGER, POSITIVE_INTEGER, SHORT,
+                    UNSIGNED_LONG, UNSIGNED_INT, UNSIGNED_SHORT, UNSIGNED_BYTE, INT -> {
+                return INT;
+            }
+            case BASE64_BINARY -> {
+                return BYTE_ARRAY;
+            }
+            default -> {
+                return typeName;
+            }
+        }
+    }
+}
---
+++ xsd-core/src/test/java/io/ballerina/xsdtorecordconverter/XSDToRecordTest.java
@@ -0,0 +1,68 @@
+package io.ballerina.xsdtorecordconverter;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class XSDToRecordTest {
+    private static final Path RES_DIR = Paths.get("src/test/resources/").toAbsolutePath();
+    private static final String XML_DIR = "xml";
+    private static final String EXPECTED_DIR = "expected";
+
+    private static Stream<Object[]> provideTestPaths() {
+        return Stream.of(
+            new Object[] {"attributes.xml", "attributes.bal"},
+            new Object[] {"attributes_with_default_values.xml", "attributes_with_default_values.bal"},
+            new Object[] {"attributes_with_fixed_values.xml", "attributes_with_fixed_values.bal"},
+            new Object[] {"complex_schema_1.xsd", "complex_schema_1.bal"},
+            new Object[] {"complex_schema_2.xsd", "complex_schema_2.bal"},
+            new Object[] {"complex_schema_3.xsd", "complex_schema_3.bal"},
+            new Object[] {"complex_schema_4.xsd", "complex_schema_4.bal"},
+            new Object[] {"complex_schema_5.xsd", "complex_schema_5.bal"},
+            new Object[] {"complex_schema_6.xsd", "complex_schema_6.bal"},
+            new Object[] {"complex_schema_7.xsd", "complex_schema_7.bal"},
+            new Object[] {"complex_type_with_extensions.xml", "complex_type_with_extensions.bal"},
+            new Object[] {"elements_simple.xml", "elements_simple.bal"},
+            new Object[] {"elements_with_complex_type.xml", "elements_with_complex_type.bal"},
+            new Object[] {"elements_with_multiple_complex_types.xml", "elements_with_multiple_complex_types.bal"},
+            new Object[] {"elements_with_sequence.xml", "elements_with_sequence.bal"},
+            new Object[] {"elements_with_required_fields.xml", "elements_with_required_fields.bal"},
+            new Object[] {"elements_with_choice.xml", "elements_with_choice.bal"},
+            new Object[] {"elements_with_occurrences.xml", "elements_with_occurrences.bal"},
+            new Object[] {"elements_with_default_values.xml", "elements_with_default_values.bal"},
+            new Object[] {"elements_with_fixed_values.xml", "elements_with_fixed_values.bal"},
+            new Object[] {"simple_type_with_restrictions.xml", "simple_type_with_restrictions.bal"},
+            new Object[] {"simple_type_with_extensions.xml", "simple_type_with_extensions.bal"},
+            new Object[] {"simple_type_with_enumerations.xml", "simple_type_with_enumerations.bal"}
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestPaths")
+    void testXsdToRecord(String xmlFilePath, String balFilePath) throws IOException {
+        validate(RES_DIR.resolve(XML_DIR).resolve(xmlFilePath), RES_DIR.resolve(EXPECTED_DIR).resolve(balFilePath));
+    }
+
+    private void validate(Path sample, Path expected) throws IOException {
+        String xmlFileContent = Files.readString(sample);
+        String result = XSDToRecord.convert(xmlFileContent);
+        String expectedValue = Files.readString(expected);
+        Assert.assertEquals(result, expectedValue);
+    }
+
+//    @org.junit.jupiter.api.Test
+//    void testXsdSchema() throws IOException {
+//        String sourceFile = "complex_schema_1.xsd";
+//        String xmlFileContent = Files.readString(RES_DIR.resolve(XML_DIR).resolve(sourceFile));
+//        String result = XSDToRecord.convert(xmlFileContent);
+//        Path destinationFile = Paths.get(sourceFile.replace(".xsd", ".bal"));
+//        Files.writeString(destinationFile, result);
+//        System.out.println(result);
+//    }
+}
